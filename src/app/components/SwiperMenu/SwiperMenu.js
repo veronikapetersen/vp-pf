3a810faa06176411861062a5b76096e7bf8062d3
Veronika P <karavannika@gmail.com>
2024-01-16 16:48:43 +0100

    swiper scrollbar

--- src/app/components/SwiperMenu/SwiperMenu.js
+++ src/app/components/SwiperMenu/SwiperMenu.js
@@ -1,28 +1,64 @@
 import classes from './SwiperMenu.module.scss';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Mousewheel, Scrollbar } from 'swiper/modules';
+// import { Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/bundle';
-import { useRef } from 'react';
+import 'swiper/scss/scrollbar';
+// import 'swiper/css/pagination';
+import { useRef, useEffect } from 'react';
 
 export default () => {
 
     const swiperRef = useRef(null);
 
-    const swiperEl = swiperRef.current;
+    const handleSlideChange = () => {
+        const activeSlideIndex = swiperRef.current.swiper.realIndex;
+        console.log('activeSlideIndex', activeSlideIndex)
 
-    // console.log("swiper El", swiperEl)
+        const scrollbarDrag = document.querySelector('.swiper-scrollbar-drag');
+        console.log('scrollbarDrag', scrollbarDrag);
+
+        // if (scrollbarDrag) {
+        //     // Replace 'Content A', 'Content B', 'Content C', etc. with your desired content
+        //     const contentArray = ['1', '2', '3', '4'];
+        //     scrollbarDrag.style.content = `'${contentArray[activeSlideIndex]}'`;
+        // }
+
+        if (scrollbarDrag) {
+            // Remove any existing classes
+            scrollbarDrag.classList.remove('zero', 'one', 'two', 'three');
+
+            // Determine the class to add based on the activeSlideIndex
+            if (activeSlideIndex === 0) {
+                scrollbarDrag.classList.add('zero');
+            } else if (activeSlideIndex === 1) {
+                scrollbarDrag.classList.add('one');
+            } else if (activeSlideIndex === 2) {
+                scrollbarDrag.classList.add('two');
+            } else if (activeSlideIndex === 3) {
+                scrollbarDrag.classList.add('three');
+            }
+        }
+    }
+
+    // Use useEffect to handle the initial load
+    useEffect(() => {
+        handleSlideChange(); // Call the handler to set content for index 0
+    }, []);
     return (
         <Swiper
             ref={swiperRef}
             className={classes.swiper}
+            // modules={[Scrollbar, Mousewheel, Pagination]}
             modules={[Scrollbar, Mousewheel]}
-            pagination={{ clickable: true }}
+            // pagination={{ clickable: true, type: 'fraction' }}
             direction='vertical'
             scrollbar={{ draggable: true }}
             mousewheel
             slidesPerView={1}
-        // onSlideChange={() => console.log('slide change')}
+            // onSlideChange={() => console.log('slide change')}
+            onSlideChange={handleSlideChange}
         // onSwiper={(swiper) => console.log(swiper)}
         >
             <SwiperSlide className={classes.slide}>Slide 1</SwiperSlide>
