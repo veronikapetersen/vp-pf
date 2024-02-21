5baa449d4d8105ecd7825fdaf8a603bcd5b760bd
Veronika P <karavannika@gmail.com>
2024-02-21 21:31:22 +0100

    menu component responsive

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -9,22 +9,22 @@ import { useEffect, useRef } from 'react';
 
 export default function Menu() {
 
-    const menulinksRef = useRef(null);
+    const listRef = useRef(null);
 
-    // useEffect(() => {
+    useEffect(() => {
 
-    //     const menulinks = menulinksRef.current.children;
-    //     const menulinksArray = Array.from(menulinks);
+        const list = listRef.current.children;
+        const menulinksArray = Array.from(list);
 
-    //     gsap.set(menulinksArray, { opacity: 0, y: '150vh', rotateY: '-90deg' });
-    //     gsap.to(menulinksArray, { y: 0, opacity: 1, rotateY: '-45deg', stagger: 0.1, duration: 2, ease: "power3.inOut", delay: 0.2 });
+        gsap.set(menulinksArray, { opacity: 0, y: '150vh', rotateY: '-90deg' });
+        gsap.to(menulinksArray, { y: 0, opacity: 1, rotateY: '-45deg', stagger: 0.1, duration: 2, ease: "power3.inOut", delay: 0.2 });
 
-    // }, []);
+    }, []);
 
     return (
         <div className={classes['stage-perspective']}>
             <div className={classes.stage}>
-                <ul ref={menulinksRef} className={classes.list}>
+                <ul ref={listRef} className={classes.list}>
                     {ProjectsData.map((project) => (
                         <li key={project.id} className={`${classes.menulinkWrapper} ${archivo_black.className}`}>
                             <Link data-text={project.year + ' /'} href={`/projects/${project.slug}`}>{project.title}</Link>
