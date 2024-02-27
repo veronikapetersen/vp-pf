0ce66f6e6caee71c0976b182879e8c132628fe2e
Veronika P <karavannika@gmail.com>
2024-02-27 22:06:49 +0100

    mobile menu

--- src/components/FoldMenu/FoldMenu.js
+++ src/components/FoldMenu/FoldMenu.js
@@ -1,5 +1,7 @@
+import Link from "next/link";
 import gsap from 'gsap';
 
+import { roboto, rubik } from '@/utils/fonts';
 import classes from './FoldMenu.module.scss';
 
 import { useEffect, useRef, useState } from 'react';
@@ -9,11 +11,19 @@ export default function FoldMenu() {
 
     const stairsRef = useRef(null);
     const menuRef = useRef(null);
-
     const [menuIsOpen, setMenuIsOpen] = useState(false);
 
+
+
+    const logoRef = useRef(null);
+    const column1 = useRef(null);
+    const column2 = useRef(null);
+
+    const mobileMenuRef = useRef(null);
+    const [mobileMenuIsOpen, setMobileMenuIsOpen] = useState(false);
+
     const openMenuHandler = () => {
-        setMenuIsOpen(true);
+        setMobileMenuIsOpen(true);
         const stairs = Array.from(stairsRef.current.children);
         gsap.set(stairs, { y: '-100vh', height: '100vh' });
         gsap.to(stairs, {
@@ -24,7 +34,14 @@ export default function FoldMenu() {
             ease: "power3.inOut",
         });
 
-        const menuItems = Array.from(menuRef.current.children);
+        const menuItems = Array.from(mobileMenuRef.current.children);
+
+        gsap.set(mobileMenuRef.current, {
+            width: '100%',
+            height: '100vh',
+            duration: 0.1,
+        })
+
         gsap.to(menuItems, {
             opacity: 1,
             delay: 0.6,
@@ -42,53 +59,89 @@ export default function FoldMenu() {
             duration: 0.8,
             delay: 0.4,
             ease: "power3.inOut",
-            onComplete: () => { console.log("out", menuIsOpen) }
         })
-        setMenuIsOpen(false);
+        setMobileMenuIsOpen(false);
 
-        const menuItems = Array.from(menuRef.current.children);
+        const menuItems = Array.from(mobileMenuRef.current.children);
+
+
         gsap.to(menuItems, {
             opacity: 0,
             duration: 0.6,
             stagger: 0.1,
             ease: "power3.inOut",
+            onComplete: () => {
+                gsap.set(mobileMenuRef.current, {
+                    width: '0',
+                    height: '0',
+                    duration: 0.1,
+                })
+            }
         })
     }
 
     return (
-        <>
-            <div>
-                <div className={classes.burgerIconWrapper}>
-                    <div className={classes.burgerIcon}>
-                        {menuIsOpen ?
-                            <div onClick={closeMenuHandler} className={classes.close}>
-                                <svg width="68" height="68" viewBox="0 0 68 68" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M1.5 1.5L67 67" stroke="black" /> <path d="M66.5 1L0.999997 66.5" stroke="black" /> </svg>
-                            </div> :
-                            <div onClick={openMenuHandler} className={classes.open}>
-                                <div className={classes.line}></div>
-                                <div className={classes.line}></div>
-                                <div className={classes.line}></div>
-                            </div>
-                        }
-                    </div>
-                </div>
-
-                <div ref={menuRef} className={classes.menu}>
-                    <div className={classes.menuItem}>Projects</div>
-                    <div className={classes.menuItem}>Agency</div>
-                    <div className={classes.menuItem}>Contact</div>
-                </div>
-
-                <div ref={stairsRef} className={classes.stairs}>
-                    <div className={classes.stair}></div>
-                    <div className={classes.stair}></div>
-                    <div className={classes.stair}></div>
-                    <div className={classes.stair}></div>
-                    <div className={classes.stair}></div>
-                </div>
-
+        <header className={classes.header}>
+            <div className={`${classes['logo-wrapper']} ${roboto.className}`}>
+                <div ref={logoRef} className={classes.logo}><Link href="/" >VP</Link></div>
             </div>
-        </>
-    )
+            <div className={classes.burgerIconWrapper}>
+                <div className={classes.burgerIcon}>
+                    {mobileMenuIsOpen ?
+                        <div onClick={closeMenuHandler} className={classes.close}>
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" /></svg>
+                        </div> :
+                        <div onClick={openMenuHandler} className={classes.open}>
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" /></svg>
+                        </div>
+                    }
+                </div>
+            </div>
 
+
+            <div ref={menuRef} className={classes.menu}>
+                <div ref={column1} className={classes.column}>
+                    <nav className={classes.nav}>
+                        <ul className={classes['nav-list']}>
+                            <li className={classes['nav-list-item']}>
+                                <div>Veronika Veronika</div>
+                            </li>
+                            <li className={classes['nav-list-item']}>
+                                <div>Frontend Developer</div>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+
+                <div ref={column2} className={classes.column}>
+                    <nav className={classes.nav}>
+                        <ul className={classes['nav-list']}>
+                            <li className={classes['nav-list-item']}>
+                                <a href="/">LinkedIn</a>
+                            </li>
+                            <li className={`${classes['nav-list-item']} ${classes.email}`}>
+                                <a href="/">myemailhere@gmail.com</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+
+            <div ref={mobileMenuRef} className={`${classes.menu} ${classes['menu-mobile']} ${rubik.className}`}>
+                <div className={classes['menu-item']}>Veronika Petersen</div>
+                <div className={classes['menu-item']}>frontend developer</div>
+                <div className={classes['menu-item']}><a href="/sss">linkedin</a></div>
+                <div className={classes['menu-item']}><a href="/">myemailhere@gmail.com</a></div>
+            </div>
+
+            <div ref={stairsRef} className={classes.stairs}>
+                <div className={classes.stair}></div>
+                <div className={classes.stair}></div>
+                <div className={classes.stair}></div>
+                <div className={classes.stair}></div>
+                <div className={classes.stair}></div>
+            </div>
+
+        </header >
+    )
 }
