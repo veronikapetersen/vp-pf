8e866c27d66bd7a86ff9d37f86fc121daace45c1
Veronika Petersen <karavannika@gmail.com>
2025-11-04 21:12:11 +0100

    disable pointer events until animation ends

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,48 +1,59 @@
-import classes from './Menu.module.scss';
-import Link from 'next/link';
+import classes from "./Menu.module.scss";
+import Link from "next/link";
 
-import { archivo_black } from '@/utils/fonts';
+import { archivo_black } from "@/utils/fonts";
 import ProjectsData from "@/projects-data/ProjectsData";
 
-import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
-import { useRef } from 'react';
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
 
 export default function Menu() {
+  const wrapperRef = useRef(null);
+  const listRef = useRef(null);
 
-    const wrapperRef = useRef(null);
-    const listRef = useRef(null);
+  useGSAP(
+    () => {
+      const list = listRef.current;
+      const menulinksArray = Array.from(list.children);
 
-    useGSAP(() => {
+      list.style.pointerEvents = "none";
 
-        const list = listRef.current.children;
-        const menulinksArray = Array.from(list);
+      gsap.set(menulinksArray, { opacity: 0, y: "150vh", rotateY: "-90deg" });
+      gsap.to(menulinksArray, {
+        y: 0,
+        opacity: 1,
+        rotateY: "-45deg",
+        stagger: 0.1,
+        duration: 2,
+        ease: "power3.inOut",
+        onComplete: () => {
+          list.style.pointerEvents = "auto";
+        },
+      });
+    },
+    { scope: wrapperRef }
+  );
 
-        gsap.set(menulinksArray, { opacity: 0, y: '150vh', rotateY: '-90deg' });
-        gsap.to(menulinksArray, {
-            y: 0, opacity: 1, rotateY: '-45deg',
-            stagger: 0.1, duration: 2, ease: "power3.inOut"
-        });
-
-    }, { scope: wrapperRef });
-
-    return (
-        <div ref={wrapperRef} className={classes['stage-perspective']}>
-            <div className={classes.stage}>
-                <ul ref={listRef} className={classes.list}>
-
-                    {ProjectsData.map((project) => (
-
-                        <li key={project.id} className={`${classes['project-li']} ${archivo_black.className}`}>
-
-                            <Link data-text={project.year + ' /'} href={`/projects/${project.slug}`}>{project.title}</Link>
-
-                        </li>
-
-                    ))}
-
-                </ul>
-            </div>
-        </div>
-    );
+  return (
+    <div ref={wrapperRef} className={classes["stage-perspective"]}>
+      <div className={classes.stage}>
+        <ul ref={listRef} className={classes.list}>
+          {ProjectsData.map((project) => (
+            <li
+              key={project.id}
+              className={`${classes["project-li"]} ${archivo_black.className}`}
+            >
+              <Link
+                data-text={project.year + " /"}
+                href={`/projects/${project.slug}`}
+              >
+                {project.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
